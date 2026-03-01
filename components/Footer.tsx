a451a4d2a1fe063cd0b0b71dbe64ce0677e83629
Jannatul <limajannat1675@gmail.com>
2026-03-02 01:52:56 +0600

    added polishing to footer section

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <footer className="py-8 text-center bg-rose-700 text-white">
       <p>© 2026 Noor-E-Eid Henna Fest</p>
       <p className="text-sm mt-2">
-        Designed with 🤍 for Eid Celebration
+        Built with 🤍 by <a className="text-amber-300 hover:text-amber-500" href="https://jannatullima.vercel.app/">Jannatul Lima</a>
       </p>
     </footer>
   );
